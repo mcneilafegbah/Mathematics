697f316811d5b7336f8516c4555be49fdac2d055
mcneilafegbah <mcneilko@gmail.com>
2024-03-10 19:03:24 +0000

    Tidy up up for user

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,14 @@ document.addEventListener("DOMContentLoaded", function() {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
             }
-        });
+        })
     }
+            document.getElementById("answer-box").addEventListener("keydown", function(event) {
+                if (event.key === "Enter") {
+                    checkAnswer();
+                }
+        });
+    
 
     runGame("addition");
    
@@ -22,6 +28,8 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+    document.getElementById ("answer-box").value = "";
+    document.getElementById ("answer-box").focus();
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
